1c5e437e4ff33cb414d945c14f4b8ac9df3b896a
yuehuanghun <blusky_fj@126.com>
2024-02-06 23:10:04 +0800

    增加预置typehandler

---
+++ mybatis-milu-spring/src/main/java/com/yuehuanghun/mybatis/milu/ext/typehandler/Boolean2IntTypeHandler.java
@@ -0,0 +1,56 @@
+package com.yuehuanghun.mybatis.milu.ext.typehandler;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+/**
+ * jdbc数字类型与java布尔类型转换
+ */
+public class Boolean2IntTypeHandler extends BaseTypeHandler<Boolean> {
+
+	@Override
+	public void setNonNullParameter(PreparedStatement ps, int i, Boolean parameter, JdbcType jdbcType)
+			throws SQLException {
+		if(parameter == null) {
+			ps.setNull(i, Types.INTEGER);
+		} else if(parameter.booleanValue()) {
+			ps.setInt(i, (short) 1);
+		} else {
+			ps.setInt(i, (short) 0);
+		}
+	}
+
+	@Override
+	public Boolean getNullableResult(ResultSet rs, String columnName) throws SQLException {
+		if(rs.wasNull()) {
+			return null;
+		}
+		int value = rs.getInt(columnName);
+		return value == 0 ? Boolean.FALSE : Boolean.TRUE;
+	}
+
+	@Override
+	public Boolean getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+		if(rs.wasNull()) {
+			return null;
+		}
+		int value = rs.getInt(columnIndex);
+		return value == 0 ? Boolean.FALSE : Boolean.TRUE;
+	}
+
+	@Override
+	public Boolean getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+		if(cs.wasNull()) {
+			return null;
+		}
+		int value = cs.getInt(columnIndex);
+		return value == 0 ? Boolean.FALSE : Boolean.TRUE;
+	}
+
+}
