843838909f8b56e1ddce9896e031309e38dc14cc
yuehuanghun <blusky_fj@126.com>
2025-11-30 18:38:48 +0800

    VEntity加入一些参数校验

--- mybatis-milu/src/main/java/com/yuehuanghun/mybatis/milu/metamodel/VEntity.java
+++ mybatis-milu/src/main/java/com/yuehuanghun/mybatis/milu/metamodel/VEntity.java
@@ -22,9 +22,11 @@ import java.util.Map;
 
 import com.yuehuanghun.mybatis.milu.filler.Filler;
 import com.yuehuanghun.mybatis.milu.metamodel.Entity.Attribute;
+import com.yuehuanghun.mybatis.milu.metamodel.Entity.FunctionAttribute;
 import com.yuehuanghun.mybatis.milu.metamodel.Entity.Index;
 import com.yuehuanghun.mybatis.milu.metamodel.ref.Reference;
 import com.yuehuanghun.mybatis.milu.tool.Assert;
+import com.yuehuanghun.mybatis.milu.tool.StringUtils;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -32,25 +34,25 @@ import lombok.Setter;
 @Getter
 @Setter
 public class VEntity {
-	
+	/** 实体名。作标识使用 */
 	private String name;
-	
+	/** 对应的数据库表名 */
 	private String tableName;
-	
+	/** 数据表所在的数据库catalog，可空。 */
 	private String catalog;
-	
+	/** 数据表所在的数据库schema，可空。 */
 	private String schema;
-	
+	/** 属性与表字段的映射与配置，不可空 */
 	private List<Attribute> attributes = new ArrayList<>();
-	
+	/** 外键引用，如果需要表间关联查询则需要设置 */
 	private final Map<String, Reference> referenceMap = new HashMap<>();
-	
+	/** 结果集绑定的POJO类，即实体类，可空，默认为Map */
 	private Class<?> javaType;
-	
+	/** 更新时自动填充配置，可空 */
 	private final List<Filler> onUpdateFillers = new ArrayList<>();
-
+	/** 新增时自动填充配置，可空 */
 	private final List<Filler> onInsertFillers = new ArrayList<>();
-	
+	/** 索引信息，可空，使用batchMerge功能时需要。 */
 	private final Map<String, Index> indexMap = new HashMap<>();
 	
 	public void addAttribute(Attribute attribute) {
@@ -88,7 +90,17 @@ public class VEntity {
 		entity.setSchema(schema);
 		entity.setJavaType(javaType);
 		
-		attributes.forEach(attr -> entity.addAttribute(attr));
+		attributes.forEach(attr -> {
+			Assert.notBlank(attr.getName(), "属性名name不能为空");
+			if(attr instanceof FunctionAttribute) {
+				if(StringUtils.isBlank(attr.getColumnName())) {
+					attr.setColumnName(attr.getName());
+				}
+			} else {
+				Assert.notBlank(attr.getColumnName(), "属性映射数据表字段名columnName值不能为空");
+			}
+			entity.addAttribute(attr);
+		});
 		referenceMap.forEach((attr, ref) -> entity.addReference(attr, ref));
 		onUpdateFillers.forEach(filler -> entity.getOnUpdateFillers().add(filler));
 		onInsertFillers.forEach(filler -> entity.getOnInsertFillers().add(filler));
